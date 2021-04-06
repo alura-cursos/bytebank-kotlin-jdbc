2e5c35963bcff9f14c60651aacbd0192bdefd270
Alex Felipe <alexfelipevieira@gmail.com>
2021-04-06 10:12:29 -0300

    projeto inicial

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "br.com.alura.bytebank"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+//    implementation("mysql:mysql-connector-java:8.0.15")
+    testImplementation("junit", "junit", "4.12")
+}
